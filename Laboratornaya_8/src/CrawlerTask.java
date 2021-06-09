8214b1f937654f263fe8afb3082dc789277addbf
Owonin <78658776+Owonin@users.noreply.github.com>
2021-06-09 17:36:40 +0300

    Add files via upload

---
+++ Laboratornaya_8/src/CrawlerTask.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class CrawlerTask implements Runnable {
+    public URLDepthPair pair;
+    public URLPool pool;
+
+    CrawlerTask(URLPool pool) {
+        this.pool = pool;
+    }
+
+    public void run() {
+        pair = pool.get();
+
+        ArrayList<String> links = new ArrayList<String>();
+        links = Crawler.getURLs(pair);
+
+        for (String link : links) {
+            URLDepthPair newPair = new URLDepthPair(link, pair.getDepth() + 1);
+            if (!pool.getSeenList().contains(link)) {
+                pool.put(newPair);
+            }
+        }
+    }
+}
